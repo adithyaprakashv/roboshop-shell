beed821cabd8e67f38cce941f38031b752d7d2b6
adithyaprakash <adithya@gmail.com>
2023-09-23 19:33:17 +0530

    created mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 DATE=$(date +%F)
 LOGSDIR=/tmp
 # /home/centos/shellscript-logs/script-name-date.log
@@ -10,13 +9,11 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
-
 if [ $USERID -ne 0 ];
 then
     echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 fi
-
 VALIDATE(){
     if [ $1 -ne 0 ];
     then
@@ -26,28 +23,15 @@ VALIDATE(){
         echo -e "$2 ... $G SUCCESS $N"
     fi
 }
-
-
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
-
 VALIDATE $? "Copied MongoDB repo into yum.repos.d"
-
 yum install mongodb-org -y &>> $LOGFILE
-
 VALIDATE $? "Installation of MongoDB"
-
 systemctl enable mongod &>> $LOGFILE
-
 VALIDATE $? "Enabling MongoDB"
-
 systemctl start mongod &>> $LOGFILE
-
 VALIDATE $? "Starting MongoDB"
-
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
-
 VALIDATE $? "Edited MongoDB conf"
-
 systemctl restart mongod &>> $LOGFILE
-
 VALIDATE $? "Restarting MonogoDB"
