a985a691558436206ccf262c686de2a5711da256
adithyaprakash <adithya@gmail.com>
2023-09-23 17:19:41 +0530

    created mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,64 +1,46 @@
-#! /bin/bash
+#!/bin/bash
 
-# logs Validation script
 DATE=$(date +%F)
 LOGSDIR=/tmp
-#LOGSDIR=/home/centos/shellscript-logs
+# /home/centos/shell-script/script-name-date.log
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
-
-#color codes
+USERID=$(id -u)
 R="\e[31m"
-G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
+G="\e[32m"
 
-#user access
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ];
 then 
-    echo -e "$R ERROR :: Please do install this with a root user $N"
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
-#else
-#   echo "INFO :: You are a root user"    
 fi
 
-#validate through function VALIDATE
-VALIDATE()
-{    
-    if [ $1 -ne 0 ]
-    then 
-        echo -e "$2...$R FAILURE $N"
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then   
+        echo -e "$2 .. $R FAILURE $N"
         exit 1
     else
-        echo -e "$2...$G SUCCESS $N"
+        echo -e "$2 .. $G SUCCESS $N"
     fi
 }
 
-#copy mongo.repo file with vim path as in roboshop documentation
-#give validations for each ie copied & pasted from mongodb.md in roboshop documentation
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "Copied MongoDB repo into yum.repos.d"
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+yum install mongodb-org -y &>> $LOGFILE
+VALIDATE $? "Installation of MongoDB"
 
-VALIDATE $? "copied mongodb repo into yum.repos.d"
-
-yum install mongodb-org -y &>>$LOGFILE
-
-VALIDATE $? "Installing MongoDB"
-
-systemctl enable mongod &>>$LOGFILE
-
+systemctl enable mongod &>> $LOGFILE
 VALIDATE $? "Enabling MongoDB"
 
-systemctl start mongod &>>$LOGFILE
-
+systemctl start mongod &>> $LOGFILE
 VALIDATE $? "Starting MongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+VALIDATE $? "Edited MongoDB conf"
 
-VALIDATE $? "edited Mongo Conf"
-
-systemctl restart mongod &>>$LOGFILE
-
-VALIDATE $? "Restarting MongoDB"
+systemctl restart mongod &>> $LOGFILE
+VALIDATE $? "Restarting MonogoDB"
