ff3726338f45b56a7b9d822a5c4431f73b81b04c
adithyaprakash <adithya@gmail.com>
2023-09-23 19:25:37 +0530

    created mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -2,50 +2,52 @@
 
 DATE=$(date +%F)
 LOGSDIR=/tmp
-# /home/centos/shell-script/script-name-date.log
+# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
+G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
-G="\e[32m"
 
 if [ $USERID -ne 0 ];
-then 
+then
     echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ];
-    then   
-        echo -e "$2 .. $R FAILURE $N"
+    then
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2 .. $G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-#copy mongo.repo file with vim path as in roboshop documentation
-#give validations for each ie copied & pasted from mongodb.md in roboshop documentation
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
-VALIDATE $? "copied mongodb repo into yum.repos.d"
+VALIDATE $? "Copied MongoDB repo into yum.repos.d"
 
-yum install mongodb-org -y &>>$LOGFILE
+yum install mongodb-org -y &>> $LOGFILE
 
-VALIDATE $? "Installing MongoDB"
+VALIDATE $? "Installation of MongoDB"
 
 systemctl enable mongod &>> $LOGFILE
+
 VALIDATE $? "Enabling MongoDB"
 
 systemctl start mongod &>> $LOGFILE
+
 VALIDATE $? "Starting MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+
 VALIDATE $? "Edited MongoDB conf"
 
 systemctl restart mongod &>> $LOGFILE
+
 VALIDATE $? "Restarting MonogoDB"
