06e59e5fdcd499d07c5f65bc4b33668569bdddcb
adithyaprakash <adithya@gmail.com>
2023-09-26 14:14:50 +0530

    added server

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,7 +4,7 @@ NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping"
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-01a8230a3a293d203
-
+DOMAIN_NAME=adithyaprakash.online
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
@@ -19,4 +19,17 @@ do
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
+    
+    aws route 53 change-resource-record-sets --hosted-zone-id Z08723493UEONZLXFQQ86 --change-batch '
+{
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
 done
