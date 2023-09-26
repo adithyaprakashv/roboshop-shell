331c44413f44d4c66ccefb46dade5763cddbd3e0
adithyaprakash <adithya@gmail.com>
2023-09-26 13:46:22 +0530

    added server

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
-
-NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+#if mysql or mongodb instance-type should be t3.medium , for all others it is t2.micro
 
 for i in "${NAMES[@]}"
 do 
-    echo "NAME: $i"
+	if [[ $i == "mongodb" || $i = "mysql" ]] 
+	then
+		INSTANCE_TYPE="t3.medium"
+	else
+		INSTANCE_TYPE="t2.micro"
+	fi
+    echo "creating $i instances"
+	IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids 
+	$SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instances: $IP_ADDRESS"
 done
