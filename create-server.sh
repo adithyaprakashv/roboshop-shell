983eb1fbd10cb6341c6da9371a66c550e1e240bb
adithyaprakash <adithya@gmail.com>
2023-11-01 11:39:16 +0530

    ansible server

--- create-server.sh
+++ create-server.sh
@@ -3,8 +3,9 @@
 NAMES=$@
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
-SECURITY_GROUP_ID=sg-01a8230a3a293d203
+SECURITY_GROUP_ID=sg-0696eebf89b26c551
 DOMAIN_NAME=adithyaprakash.online
+HOSTED_ZONE_ID=Z0482781100GLUMQMM5UD
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
@@ -20,7 +21,7 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z08723493UEONZLXFQQ86 --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
     {
             "Changes": [{
             "Action": "CREATE",
